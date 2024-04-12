61243e88a7c33961823856d476c3da7f0c3e4988
Quách Tiến Thành <111704492+tannse@users.noreply.github.com>
2024-04-12 11:17:12 +0200

    update profile page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -8,6 +8,7 @@ import { auth } from "../firebase/config";
 export default function LoginPage() {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
+    const [userLogin, setUserlogin] = useState(false);
     const [signInWithEmailAndPassword] = useSignInWithEmailAndPassword(auth);
     const router = useRouter();
 
@@ -15,21 +16,38 @@ export default function LoginPage() {
         ev.preventDefault();
 
         try {
-            await signInWithEmailAndPassword(email, password);
-            router.push("/profile");
+            const res = await signInWithEmailAndPassword(email, password);
+            console.log({ res });
+            if (res) {
+                setTimeout(() => {
+                    router.push("/profile");
+                }, 3000);
+            }
         } catch (error) {
             console.error(error);
+        } finally {
+            setUserlogin(true);
         }
     };
 
     return (
         <section className="mt-8">
             <h1 className="text-center text-white">Login</h1>
-            {/* {checkEmail && (
+            {userLogin && (
                 <>
-                    <p>Please check your id </p>
+                    <p className="text-center my-4 text-white">
+                        Login Success please wait few second will move to next
+                        page{" "}
+                    </p>
                 </>
-            )} */}
+            )}
+            {!userLogin && (
+                <>
+                    <p className="text-center my-4 text-white">
+                        Please check your id and password again ^^
+                    </p>
+                </>
+            )}
             <form
                 onSubmit={HandleSignInSubmit}
                 className="block max-w-xs mx-auto"
@@ -39,6 +57,7 @@ export default function LoginPage() {
                     placeholder="email"
                     name="email"
                     value={email}
+                    disabled={userLogin}
                     onChange={(ev) => setEmail(ev.target.value)}
                 />
                 <input
@@ -46,6 +65,7 @@ export default function LoginPage() {
                     placeholder="password"
                     name="password"
                     value={password}
+                    disabled={userLogin}
                     onChange={(ev) => setPassword(ev.target.value)}
                 />
                 <button type="submit">Login</button>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,32 +5,67 @@ import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "@/app/firebase/config";
 
 export default function Profile() {
-    const [user, loading, error] = useAuthState(auth);
-    const router = useRouter();
+    const [name, setName] = useState("");
+    const [telefone, setTelefone] = useState("");
+    const [adress, setAdress] = useState("");
 
-    // Kiểm tra xem người dùng đã đăng nhập hay chưa
-    useEffect(() => {
-        if (!loading && !user) {
-            router.push("/login"); // Chuyển hướng đến trang đăng nhập nếu người dùng chưa đăng nhập
-        }
-    }, [user, loading]);
-
-    // Nếu đang tải, hiển thị thông báo đang tải
-    if (loading) {
-        return <div>Loading...</div>;
-    }
-
-    // Nếu có lỗi, hiển thị thông báo lỗi
-    if (error) {
-        return <div>Error: {error.message}</div>;
-    }
+    const handleSubmit = (event: { preventDefault: () => void }) => {
+        event.preventDefault();
+        // Gửi dữ liệu form đi ở đây
+        console.log("Submitted:", { name, telefone, password });
+    };
 
     return (
-        <div>
-            <h1>Welcome to the Members Page!</h1>
-            <p className="text-white text-2xl">
-                Hello, {user?.email || "Guest"}!
-            </p>
-        </div>
+        <form onSubmit={handleSubmit} className="max-w-lg mx-auto">
+            <h1 className="text-2xl font-semibold my-4">Profile</h1>
+            <div className="my-4">
+                <label className="block text-white">First Name</label>
+                <input
+                    id="name"
+                    type="text"
+                    className="form-input py-2  rounded-xl block w-full"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div className="my-4">
+                <label className="block text-white">Last Name</label>
+                <input
+                    id="name"
+                    type="text"
+                    className="form-input py-2  rounded-xl block w-full"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                />
+            </div>
+            <div className="my-4">
+                <label className="block text-white">telefone</label>
+                <input
+                    id="telefone"
+                    type="telefone"
+                    className="form-input py-2  rounded-xl block w-full"
+                    value={telefone}
+                    onChange={(e) => setTelefone(e.target.value)}
+                />
+            </div>
+            <div className="my-4">
+                <label className="block text-white">Adress</label>
+                <input
+                    id="adress"
+                    type="adress"
+                    className="form-input py-2  rounded-xl block w-full"
+                    value={adress}
+                    onChange={(e) => setAdress(e.target.value)}
+                />
+            </div>
+            <div className="flex justify-end">
+                <button
+                    type="submit"
+                    className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+                >
+                    Save
+                </button>
+            </div>
+        </form>
     );
 }
