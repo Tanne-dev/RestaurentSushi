510532772b93eda0780dbc25a178bddda7998b78
Quách Tiến Thành <111704492+tannse@users.noreply.github.com>
2024-05-30 11:56:53 +0200

    fix bugs

--- src/components/Menu/CatergoriesItems.tsx
+++ src/components/Menu/CatergoriesItems.tsx
@@ -29,20 +29,23 @@ export default function CatergoriesItems() {
         <>
             <ul className="flex flex-col list-none z-[100]">
                 {Object.keys(groupName).map((key, index) => (
-                    <li className="flex justify-between hover:bg-slate-300 duration-200 transition-background ease-in-out py-4 bg-gray-100 mt-4 drop-shadow-lg rounded-lg">
-                        <li key={key} className=" flex gap-4 ml-4 ">
+                    <li
+                        key={key}
+                        className="flex justify-between hover:bg-slate-300 duration-200 transition-background ease-in-out py-4 bg-gray-100 mt-4 drop-shadow-lg rounded-lg"
+                    >
+                        <div className=" flex gap-4 ml-4 ">
                             <span className="text-black text-[1.5rem] ml-2">
                                 {key}
                             </span>
                             <h4 className="text-black font-medium text-[1.5rem] ml-2">
                                 {groupName[key]}
                             </h4>
-                        </li>
-                        <li className=" mr-8">
+                        </div>
+                        <div className=" mr-8">
                             <div className="cursor-pointer hover:border-[0.5px] border-gray-700 rounded-md">
                                 <Ellipis />
                             </div>
-                        </li>
+                        </div>
                     </li>
                 ))}
             </ul>
